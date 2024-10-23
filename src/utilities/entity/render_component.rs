b25fd01afe994fa199d503766e93925f4262893d
Isaac <isaacloff@gmail.com>
2024-10-24 00:19:58 +0100

    fixed model matrix and added testing

--- src/utilities/entity/render_component.rs
+++ src/utilities/entity/render_component.rs
@@ -5,8 +5,10 @@ use super::{
 };
 
 use cgmath::{
-    Deg, InnerSpace, Matrix4, Quaternion, Rotation3, SquareMatrix, Vector3, Vector4
+    Deg, InnerSpace, Matrix3, Matrix4, Quaternion, Rotation3, SquareMatrix, Vector3, Vector4
 };
+
+use log::warn;
 
 #[derive(Debug)]
 pub struct InstanceRenderComponent {
@@ -19,8 +21,8 @@ pub struct InstanceRenderComponent {
     pub local_rotation: Quaternion<FloatPrecision>,
     pub global_rotation: Quaternion<FloatPrecision>,
 
-    pub local_scale: Matrix4<FloatPrecision>,
-    pub global_scale: Matrix4<FloatPrecision>,
+    pub local_scale: Vector3<FloatPrecision>,
+    pub global_scale: Vector3<FloatPrecision>,
 
     pub tint: Vector4<FloatPrecision>,
     pub highlight: Vector4<FloatPrecision>,
@@ -30,19 +32,20 @@ impl Default for InstanceRenderComponent {
     fn default() -> Self {
         Self {
             visible: true,
+			// want alternative..
             object_id: ObjectId("Used Default(), use 'new' and pass the object id".to_string()),
 
-            local_translation: Vector3 { x: 0.0, y: 0.0, z: 0.0 },
-            global_translation: Vector3 { x: 0.0, y: 0.0, z: 0.0 },
+            local_translation: Vector3::from_value(0.0),
+            global_translation: Vector3::from_value(0.0),
 
-            local_rotation: Quaternion::new(0.0, 0.0, 0.0, 1.0),
-            global_rotation: Quaternion::new(0.0, 0.0, 0.0, 1.0),
+            local_rotation: Quaternion::from(Matrix3::identity()),
+            global_rotation: Quaternion::from(Matrix3::identity()),
 
-            local_scale: Matrix4::identity(),
-            global_scale: Matrix4::identity(),
+            local_scale: Vector3::from_value(1.0),
+            global_scale: Vector3::from_value(1.0),
 
-            tint: Vector4 { x: 1.0, y: 1.0, z: 1.0, w: 1.0 },
-            highlight: Vector4 { x: 0.0, y: 0.0, z: 0.0, w: 0.0 }
+            tint: Vector4::from_value(0.0), 
+            highlight: Vector4::from_value(0.0), 
         }
     }
 }
@@ -64,36 +67,100 @@ impl InstanceRenderComponent {
     pub fn model_matrix(&self) -> Matrix4<FloatPrecision> {
         Matrix4::from_translation(self.global_translation) *
         Matrix4::from(self.global_rotation) *
-        self.global_scale *
+        Matrix4::from_nonuniform_scale(self.global_scale.x, self.global_scale.y, self.global_scale.z) *
         Matrix4::from_translation(self.local_translation) *
         Matrix4::from(self.local_rotation) *
-        self.local_scale
+        Matrix4::from_nonuniform_scale(self.local_scale.x, self.local_scale.y, self.local_scale.z)
     }
 
-    pub fn local_stretch(&mut self, other: &Vector3<FloatPrecision>) {
-        self.local_scale = Matrix4::from_nonuniform_scale(other.x, other.y, other.z) * self.local_scale;
-    }
-
-    pub fn global_stretch(&mut self, other: &Vector3<FloatPrecision>) {
-        self.global_scale = Matrix4::from_nonuniform_scale(other.x, other.y, other.z) * self.global_scale;
-    }
-
-    pub fn local_rotate(&mut self, angle: Deg<FloatPrecision>, axis: Vector3<FloatPrecision>) {
+    pub fn local_rotate(&mut self, angle: &Deg<FloatPrecision>, axis: &Vector3<FloatPrecision>) {
         let rotation_quat = Quaternion::from_axis_angle(axis.normalize(), angle);
         self.local_rotation = rotation_quat * self.local_rotation;
     }
 
-    pub fn global_rotate(&mut self, angle: Deg<FloatPrecision>, axis: Vector3<FloatPrecision>) {
+    pub fn global_rotate(&mut self, angle: &Deg<FloatPrecision>, axis: &Vector3<FloatPrecision>) {
         let rotation_quat = Quaternion::from_axis_angle(axis.normalize(), angle);
         self.global_rotation = rotation_quat * self.global_rotation;
     }
+
+	pub fn model_vertex(&self, vertex: Vector4<FloatPrecision>) -> Vector4<FloatPrecision> {
+		if vertex.w != 1.0 { warn!("Vertex taken as direction, translations won't apply") }
+		self.model_matrix() * vertex
+	}
 }
 
 #[cfg(test)]
 mod tests {
+	use cgmath::{Deg, Vector3, Vector4};
+
+	use super::{InstanceRenderComponent, FloatPrecision};
+
+	const EPSILON: f32 = 0.000001;
+	fn approx_equal(got: Vector3<FloatPrecision>, expected: Vector3<FloatPrecision>) {
+		assert!(
+			(got.x - expected.x).abs() < EPSILON &&
+			(got.y - expected.y).abs() < EPSILON &&
+			(got.z - expected.z).abs() < EPSILON,
+			"{}", &format!("Expected: {:?}, Got: {:?}", expected, got)
+		)
+	}
+
     #[test]
-    fn todo() {
-        todo!()
-    }
+    fn rotating() {
+        let mut r = InstanceRenderComponent::default();
+		r.global_rotate(&Deg(90.0), &Vector3::unit_y());
+		let v = r.model_matrix() * Vector4::unit_x();
+		approx_equal(v.truncate(), -Vector3::unit_z());
+
+		r.local_rotate(&Deg(45.0), &Vector3::unit_y());
+		let v = r.model_matrix() * Vector4::unit_x();
+		approx_equal(v.truncate(), Vector3::new(-0.7071068, 0.0, -07071068));
+	}
+
+	#[test]
+	fn local_rotating() {
+		let mut r = InstanceRenderComponent::default();
+		r.local_rotate(&Deg(90.0), &Vector3::unit_y());
+		let v = r.model_matrix() * Vector4::unit_x();
+		approx_equal(v.truncate(), -Vector3::unit_z());
+	}
+
+	#[test]
+	fn scaling() {
+		let mut r = InstanceRenderComponent::default();
+		r.global_scale = Vector3::new(2.0, 2.0, 2.0);
+		let v = r.global_matrix() * Vector4::unit_x();
+		approx_equal(v.truncate(), Vector3::new(2.0, 0.0, 0.0));
+		r.local_scale *= 2.0;
+		let v = r.model_matrix() * Vector4::new(1.0, 1.0, 1.0, 0.0);
+		approx_equal(v.truncate(), Vector3::new(4.0, 4.0, 4.0));
+	}
+
+	#[test]
+	fn translating() {
+		let mut r = InstanceRenderComponent::default();
+		r.global_translation = Vector3::unit_x();
+		let v = r.model_matrix() * Vector4::new(1.0, 0.0, 0.0, 1.0);
+		approx_equal(v.truncate(), Vector3::new(2.0, 0.0, 0.0));
+		r.local_translation = Vector3::unit_y();
+		let v = r.model_matrix() * Vector4::new(0.0, 0.0, 0.0, 0.0);
+		approx_equal(v.truncate(), Vector3::new(1.0, 1.0, 0.0));
+	}
+
+	#[test]
+	fn all() {
+		let mut r = InstanceRenderComponent::default();
+		r.local_scale = Vector3::new(2.0, 0.5, 1.0);
+		r.local_rotate(&Deg(90.0), &Vector3::unit_z());
+		r.local_translation = Vector3::new(3.0, 2.0, 0.0);
+		r.global_scale = Vector3::new(1.0, 1.0, 3.0);
+		r.global_rotate(&Deg(45.0), &Vector3::unit_x());
+		r.global_translation = Vector3::new(0.0, 0.0, 1.0);
+
+		// w = 0.0 -> direction -> translation doesn't apply
+		// w = 1.0 -> position -> translation doesn't apply
+		let v = r.model_vertex(Vector4::new(1.0, 0.0, 0.0, 1.0));
+		approx_equal(v.truncate(), Vector3::new(3.0, 2.8284268, 3.8284273));
+	}
 }
 
